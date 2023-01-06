d8c2310933321334d2f128d89f2e5c3d5da4bc2f
Devansh <guyawsm@gmail.com>
2023-01-06 12:45:19 +0530

    new app

---
+++ src/Mainpage.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+function Mainpage() {
+    const [data, setData] = useState("");
+    async function fetchData(){
+        const res = await axios.get("https://timetable-amber.vercel.app/")
+        console.log(res)
+        setData(res.data)
+    }
+    useEffect(()=>{
+      fetchData()
+    },[])
+
+  return (
+    <div className='msg'>
+      <h1>{data.Subject}</h1>
+      <h1>{data.room}</h1>
+      <h1>{data.time}</h1>
+      
+    </div>
+  )
+}
+
+export default Mainpage
