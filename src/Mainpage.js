f9edeb5a7d2b59df6ec2b171372937025c24511a
Devansh <guyawsm@gmail.com>
2023-01-06 17:21:09 +0530

    res

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -2,22 +2,23 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 function Mainpage() {
-    const [data, setData] = useState("");
-    async function fetchData(){
-        const res = await axios.get("https://timetable-amber.vercel.app/")
-        console.log(res)
-        setData(res.data)
-    }
-    useEffect(()=>{
-      fetchData()
-    },[])
+  const [data, setData] = useState("");
+  async function fetchData() {
+    const res = await axios.get("https://timetable-amber.vercel.app/")
+    console.log(res)
+    setData(res.data)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
-    <div className='msg'>
-      <h1>{data.Subject}</h1>
-      <h1>{data.room}</h1>
-      <h1>{data.time}</h1>
-      
+    <div>
+      <div className='msg'>
+        <h1>{data.Subject}</h1>
+        <h1>{data.room}</h1>
+        <h1>{data.time}</h1>
+      </div>
     </div>
   )
 }
