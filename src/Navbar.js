d250b59dc89999ef164847aea955394dabd8f3d0
Devansh <guyawsm@gmail.com>
2023-01-12 14:02:19 +0530

    cghgh

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const navstl = {
+    position:'sticky',
+    top:"1%"
+  }
   const ulstyle = {
     listStyle: "none",
     display:"flex",
     justifyContent:"space-between"
   }
   return (
-    <div>
+    <div style={navstl}>
       <ul style={ulstyle}>
         <li>
           <Link className='link' to='/'>Current Class</Link>
